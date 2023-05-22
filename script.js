7751b4d7fd647286a84279ded90965851b8a8fce
hudyakow.dev <108136177+hudyakow@users.noreply.github.com>
2023-05-22 08:39:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
+/*window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      document.getElementById("sec" + 1).style.display = "block";
+    } else {
+      document.getElementById("sec1").style.display = "none";
+    }
+};*/
+
+
+/*window.onscroll = function(ev) {
+  for (var number = 1; number < 5; number++) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      document.getElementById("sec" + number).style.display = "block";
+    } else {
+      document.getElementById("sec" + number).style.display = "none";
+    }
+  }
+};
+*/
